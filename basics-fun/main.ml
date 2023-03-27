79d76e67b9aed88f045a885e24dd7f18e09a569a
diegopacheco <diego.pacheco.it@gmail.com>
2023-03-27 00:56:30 -0700

    ocml basics

--- basics-fun/main.ml
+++ basics-fun/main.ml
@@ -1,5 +1,12 @@
 let x = 10;;
 print_int(x);;
+print_string("\n");;
 
 let pi = 4.0 *. atan 1.0;;
 print_float(pi);;
+print_string("\n");;
+
+let square x = x *. x;;
+let result = square (sin pi) +. square (cos pi);;
+print_float(result);;
+print_string("\n");;
